daa586fdadaba2cd7467d7a6eed179764ffb3675
William Wang <williamwyj1@gmail.com>
2025-04-19 22:50:05 -0700

    added function to calculate for the portfolio, current value, perc change, table to display on the home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/portfolio";
 
 function App() {
+  const [stocks, setStocks] = useState([]);
+  useEffect(() => {
+    fetchStocks();
+    console.log("stocks", stocks);
+  }, []);
+
+  const fetchStocks = async () => {
+    const result = await axios.get(API_URL);
+    calcStockData(result.data);
+    setStocks(result.data);
+  };
+  const calcStockData = (inputData) => {
+    return inputData.map((item) => {
+      console.log("item", item);
+      item.stockValue = (item.Quantity * item.Close).toFixed(2);
+      item.percChange = (
+        ((item.Close - item.Purchase) / item.Purchase) *
+        100
+      ).toFixed(2);
+      item.Weight = item.Weight.toFixed(4);
+      return item;
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <table>
+        <thead>
+          <tr>
+            <th>Ticker</th>
+            <th>Share Price</th>
+            <th>Quantity</th>
+            <th>Cost per share</th>
+            <th>Current Value</th>
+            <th>Percentage Change</th>
+            <th>Weight</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stocks.map((item, index) => (
+            <tr key={index}>
+              <td>{item.Ticker}</td>
+              <td>${item.Close}</td>
+              <td>{item.Quantity}</td>
+              <td>${item.Purchase}</td>
+              <td>${item.stockValue}</td>
+              <td>{item.percChange}%</td>
+              <td>{item.Weight}%</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
