fde30b36ee11f377fd9b40e4999b710cf9aa1f19
William Wang <williamwyj1@gmail.com>
2025-04-24 21:48:14 -0700

    updated html and styling for queries pop up

--- src/App.js
+++ src/App.js
@@ -1,47 +1,70 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Modal from "./components/Modal";
+import "./App.css";
 
-const API_URL = "http://localhost:5000/portfolio";
+const STOCKS_URL = "http://localhost:5000/portfolio";
+const sqlqueries_URL = "http://localhost:5000/sqlqueries";
 
 function App() {
   const [stocks, setStocks] = useState([]);
+  const [sqlQueries, setSqlQueries] = useState([]);
 
   useEffect(() => {
     fetchStocks();
-    console.log("stocks", stocks);
+    fetchSqlqueries();
   }, []);
 
   const fetchStocks = async () => {
-    const result = await axios.get(API_URL);
+    const result = await axios.get(STOCKS_URL);
     calcStockData(result.data);
     setStocks(result.data);
+  };
+  const fetchSqlqueries = async () => {
+    const result = await axios.get(sqlqueries_URL);
+    setSqlQueries(result.data);
+  };
+  const fetchSqlquery = async (queryid) => {
+    const result = await axios.get(
+      `http://localhost:5000/query/query${queryid}`
+    );
+    console.log("result.data", result.data);
+    return result.data;
   };
   const calcStockData = (inputData) => {
     return inputData.map((item) => {
       console.log("item", item);
-      item.stockValue = (item.Quantity * item.Close).toFixed(2);
+      item.stockValue = (item.quantity * item.close).toFixed(2);
       item.percChange = (
-        ((item.Close - item.Purchase) / item.Purchase) *
+        ((item.close - item.purchase) / item.purchase) *
         100
       ).toFixed(2);
-      item.Weight = item.Weight.toFixed(4);
+      item.weight = item.weight.toFixed(4);
       return item;
     });
   };
   //Popup Modal
   const [activeItemId, setActiveItemId] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const items = [
-    { id: 1, name: "Item 1", description: "Details about item 1" },
-    { id: 2, name: "Item 2", description: "Details about item 2" },
-    { id: 3, name: "Item 3", description: "Details about item 3" },
-  ];
-
-  const activeItem = items.find((item) => item.id === activeItemId);
-
+  // const items = [
+  //   { id: 1, name: "Item 1", description: "Details about item 1" },
+  //   { id: 2, name: "Item 2", description: "Details about item 2" },
+  //   { id: 3, name: "Item 3", description: "Details about item 3" },
+  // ];
+  const [activeItem, setActiveItem] = useState(null);
+  // let activeItem = sqlQueries.find((item) => item.id === activeItemId);
+  console.log("activeItem", activeItem);
   const openPopup = (itemId) => {
+    setIsLoading(true);
     setActiveItemId(itemId);
+    let selectedItem = sqlQueries.find((item) => item.id === itemId);
+    fetchSqlquery(itemId).then((data) => {
+      setIsLoading(false);
+      console.log("query data", data);
+      selectedItem.data = data;
+      setActiveItem(selectedItem);
+    });
   };
 
   const closePopup = () => {
@@ -64,36 +87,42 @@ function App() {
         <tbody>
           {stocks.map((item, index) => (
             <tr key={index}>
-              <td>{item.Ticker}</td>
-              <td>${item.Close}</td>
-              <td>{item.Quantity}</td>
-              <td>${item.Purchase}</td>
+              <td>{item.ticker}</td>
+              <td>${item.close}</td>
+              <td>{item.quantity}</td>
+              <td>${item.purchase}</td>
               <td>${item.stockValue}</td>
               <td>{item.percChange}%</td>
-              <td>{item.Weight}%</td>
+              <td>{item.weight}%</td>
             </tr>
           ))}
         </tbody>
       </table>
       <div>
         <ul>
-          {items.map((item) => (
-            <li key={item.id}>
-              <span>{item.name}</span>
-              <button onClick={() => openPopup(item.id)}>View Details</button>
+          {sqlQueries.map((query) => (
+            <li key={query.id}>
+              <span>{query.title}</span>
+              <button onClick={() => openPopup(query.id)}>View Details</button>
             </li>
           ))}
         </ul>
-        {activeItemId !== null && (
-          <div>
+      </div>
+      {activeItemId !== null && (
+        <div className='popup'>
+          {isLoading && <p className='loadingText'>loading</p>}
+          {!isLoading && (
             <Modal
               onClose={closePopup}
-              name={activeItem.name}
+              title={activeItem.title}
               description={activeItem.description}
+              query={activeItem.query}
+              data={activeItem.data}
+              loading={isLoading}
             ></Modal>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,40 @@
+import "./Modal.css";
+
 const Modal = (props) => {
+  console.log("props.data", props.data);
+  // let headings = [];
+  // if (props.data[0]) {
+  const headings = Object.keys(props.data[0]);
+  // }
   return (
-    <div>
-      <div>{props.name}</div>
+    <div className='popup-content-container'>
+      <div>{props.title}</div>
       <div>{props.description}</div>
-      <div>{props.sql}</div>
-      <div>{props.data}</div>
+      <div>
+        <pre>{props.query}</pre>
+      </div>
+      <div>
+        {!props.isloading && (
+          <table>
+            <thead>
+              <tr>
+                {headings.map((heading, index) => {
+                  return <th key={index}>{heading}</th>;
+                })}
+              </tr>
+            </thead>
+            <tbody>
+              {props.data.map((item, index) => {
+                const rowData = headings.map((heading) => {
+                  return <td>{item[heading]}</td>;
+                });
+                return <tr key={index}>{rowData}</tr>;
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
+      {/* <div>{props.data}</div> */}
       <button onClick={props.onClose}>Close</button>
     </div>
   );
